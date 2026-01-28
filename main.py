3ca643adc936b4edd13cbd9c2f86b12b7388356c
aigerabae <155903885+aigerabae@users.noreply.github.com>
2026-01-28 17:17:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ print("Number of columns in the table: ", data.shape[1])
 print(data.dtype.names)
 
 # Use np.strings.contains to create a boolean mask for a specific values of sift, polyphen and clinvar columns
-sift_keyword = 'berry'
-polyphen_keyword = 'berry'
-clinvar_keyword = 'berry'
+#sift_keyword = 'berry'
+#polyphen_keyword = 'berry'
+clinvar_keyword = 'pathogenic'
 
-mask_pathogenicity = np.strings.contains(data['SIFT'], sift_keyword) & np.strings.contains(data['PolyPhen'], polyphen_keyword) & np.strings.contains(data['Clinvar'], clinvar_keyword) 
+mask_pathogenicity = np.strings.contains(data['CLNSIG'], clinvar_keyword) 
 pathogenic_variants = data[mask_pathogenicity]
 
 print("Number of SNPs with pathogenicity predicted by SIFT, PolyPhen, and ClinVar: ", pathogenic_variants.shape[0])
@@ -22,3 +22,7 @@ print("Number of columns in the table: ", pathogenic_variants.shape[1])
 # mask on epileptic variants among pathogenic
 mask_epilepsy = np.strings.contains(data, "epilepsy") 
 epilepsy_variants = pathogenic_variants[mask_pathogenicity]
+
+
+#CLNSIG
+#InterVar_automated
