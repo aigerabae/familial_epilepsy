bafc6ccfbba7b830572c3314a76f59cce30e70ed
aigerabae <155903885+aigerabae@users.noreply.github.com>
2026-01-28 16:13:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ sift_keyword = 'berry'
 polyphen_keyword = 'berry'
 clinvar_keyword = 'berry'
 
-mask = np.strings.contains(data['SIFT'], sift_keyword) & np.strings.contains(data['PolyPhen'], polyphen_keyword) & np.strings.contains(data['Clinvar'], clinvar_keyword) 
-pathogenic_variants = data[mask]
+mask_pathogenicity = np.strings.contains(data['SIFT'], sift_keyword) & np.strings.contains(data['PolyPhen'], polyphen_keyword) & np.strings.contains(data['Clinvar'], clinvar_keyword) 
+pathogenic_variants = data[mask_pathogenicity]
 
 print("Number of SNPs with pathogenicity predicted by SIFT, PolyPhen, and ClinVar: ", pathogenic_variants.shape[0])
 print("Number of columns in the table: ", pathogenic_variants.shape[1])
+
+# mask on epileptic variants among pathogenic
+mask_epilepsy = np.strings.contains(data, "epilepsy") 
+epilepsy_variants = pathogenic_variants[mask_pathogenicity]
