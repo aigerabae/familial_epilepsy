b72e75c5618014eaefba33340731d49c29de7f95
aigerabae <155903885+aigerabae@users.noreply.github.com>
2026-01-28 16:12:07 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# Load the TSV file, specifying the tab delimiter
+data = np.loadtxt('your_file.tsv', delimiter='\t',names=True)
+
+print(data)
+print("Number of SNPs now: ", data.shape[0])
+print("Number of columns in the table: ", data.shape[1])
+print(data.dtype.names)
+
+# Use np.strings.contains to create a boolean mask for a specific values of sift, polyphen and clinvar columns
+sift_keyword = 'berry'
+polyphen_keyword = 'berry'
+clinvar_keyword = 'berry'
+
+mask = np.strings.contains(data['SIFT'], sift_keyword) & np.strings.contains(data['PolyPhen'], polyphen_keyword) & np.strings.contains(data['Clinvar'], clinvar_keyword) 
+pathogenic_variants = data[mask]
+
+print("Number of SNPs with pathogenicity predicted by SIFT, PolyPhen, and ClinVar: ", pathogenic_variants.shape[0])
+print("Number of columns in the table: ", pathogenic_variants.shape[1])
